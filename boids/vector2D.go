c60b99acd53ef2fef926864fc90874e6f3556f87
Taylor Lohman <taylor.lohman.tl@gmail.com>
2022-05-06 11:30:13 -0400

    vector math

--- boids/vector2D.go
+++ boids/vector2D.go
@@ -28,3 +28,13 @@ func (v1 Vector2D) MultiplyValue(decimal float64) Vector2D {
 func (v1 Vector2D) DivideValue(decimal float64) Vector2D {
 	return Vector2D{v1.x / decimal, v1.y / decimal}
 }
+
+func (v1 Vector2D) Limit(lower, upper float64) Vector2D {
+	return Vector2D{
+		x: math.Min(math.Max(v1.x, lower), upper),
+		y: math.Min(math.Max(v1.y, lower), upper)}
+}
+
+func (v1 Vector2D) Distance(v2 Vector2D) float64 {
+	return math.Pow(v1.x - v2.x, y: 2) + math.Pow(v1.y - v2.y, y: 2)
+}

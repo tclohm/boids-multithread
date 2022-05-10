51251d9370a899bd42cdf357f629e3f36088c822
Taylor Lohman <taylor.lohman.tl@gmail.com>
2022-05-10 16:18:40 -0700

    pools and waitgroups

--- threadpool/shoelace.go
+++ threadpool/shoelace.go
@@ -5,24 +5,32 @@ type Point2D struct {
 	y int
 }
 
-var (r = regexp.MustCompile(`\((\d*),(\d*)\)`))
+const numberOfThreads int = 8
 
-func findArea(l string) {
-	var points []Point2D
+var (
+	r = regexp.MustCompile(`\((\d*),(\d*)\)`)
+	wg = sync.WaitGroup()
+)
 
-	for _, p := range r.FindAllStringSubmatch(l, -1) {
-		x, _ := strconv.Atoi(p[1])
-		y, _ := strconv.Atoi(p[2])
-		points = append(points, Point2D{x, y})
+func findArea(input_channel chan string) {
+	for point_string := range input_channel {
+		var points []Point2D
+
+		for _, p := range r.FindAllStringSubmatch(l, -1) {
+			x, _ := strconv.Atoi(p[1])
+			y, _ := strconv.Atoi(p[2])
+			points = append(points, Point2D{x, y})
+		}
+
+		area := 0.0
+
+		for i := 0 ; i < len(points) ; i++ {
+			a, b := points[i], points[i + 1] % len(points)
+			area +: float64(a.x * b.y) - float64(a.y * b.x)
+		}
+		fmt.Println(math.Abs(area) / 2.0)
 	}
-
-	area := 0.0
-
-	for i := 0 ; i < len(points) ; i++ {
-		a, b := points[i], points[i + 1] % len(points)
-		area +: float64(a.x * b.y) - float64(a.y * b.x)
-	}
-	fmt.Println(math.Abs(area) / 2.0)
+	waitgroup.Done()
 }
 
 func main() {
@@ -31,9 +39,17 @@ func main() {
 
 	text := string(data)
 
+	input_channel := make(chan string, 1000)
+
+	for i := 0 ; i < numberOfThreads; i++ {
+		go findArea(input_channel)
+	}
+	waitgroup.Add(numberOfThreads)
 	for _, line := range strings.Split(text, "\n") {
 		// line := "(4,10),(12,8),(10,3),(2,2)(7,5)"
-		findArea(line)
+		input_channel <- line
 	}
 
+	close(input_channel)
+	waitgroup.Wait()
 }

15b5619759bb8291aa5e3627f5ddad998a9a40cb
Taylor Lohman <taylor.lohman.tl@gmail.com>
2022-05-13 12:55:41 -0700

    working on deadlocks

---
+++ deadlocks/train.go
@@ -0,0 +1,23 @@
+package main
+
+import (
+	"github.com/tclohm/boids/deadlocks/common"
+)
+
+func MoveTrain(train *Train, distance int, crossings []*Crossing) {
+	for train.Front < distance {
+		train.Front += 1
+		for _, crossing := range crossings {
+			if train.Front == crossing.Position {
+				crossing.Intersection.Mutex.Lock()
+				crossing.Intersection.LockedBy = train.Id
+			}
+			back := train.Front - train.TrainLength
+			if back == crossing.Position {
+				crossing.Intersection.LockedBy = -1
+				crossing.Intersection.Mutex.Unlock()
+			}
+		}
+		time.Sleep(30 * time.Millisecond)
+	}
+}

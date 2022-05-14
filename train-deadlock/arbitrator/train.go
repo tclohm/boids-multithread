daf6176921612cfce2bb8a85c3e7b3fc66b42978
Taylor Lohman <taylor.lohman.tl@gmail.com>
2022-05-13 17:32:35 -0700

    abitrator

---
+++ train-deadlock/arbitrator/train.go
@@ -0,0 +1,61 @@
+package arbitrator
+
+import (
+	. "github.com/tclohm/multithreading-go/train-deadlock/common"
+	"sync"
+	"time"
+)
+
+var (
+	controller = sync.Mutex{}
+	cond       = sync.NewCond(&controller)
+)
+
+func allFree(intersectionsToLock []*Intersection) bool {
+	for _, it := range intersectionsToLock {
+		if it.LockedBy >= 0 {
+			return false
+		}
+	}
+	return true
+}
+
+func lockIntersectionsInDistance(id, reserveStart int, reserveEnd int, crossings []*Crossing) {
+	var intersectionsToLock []*Intersection
+	for _, crossing := range crossings {
+		if reserveEnd >= crossing.Position &&
+			reserveStart <= crossing.Position &&
+			crossing.Intersection.LockedBy != id {
+			intersectionsToLock = append(intersectionsToLock, crossing.Intersection)
+		}
+	}
+	controller.Lock()
+	for !allFree(intersectionsToLock) {
+		cond.Wait()
+	}
+	for _, it := range intersectionsToLock {
+		it.LockedBy = id
+		time.Sleep(10 * time.Millisecond)
+	}
+	controller.Unlock()
+}
+
+func MoveTrain(train *Train, distance int, crossings []*Crossing) {
+	for train.Front < distance {
+		train.Front += 1
+		for _, crossing := range crossings {
+			if train.Front == crossing.Position {
+				lockIntersectionsInDistance(train.Id, crossing.Position, crossing.Position+train.TrainLength, crossings)
+			}
+			back := train.Front - train.TrainLength
+			if back == crossing.Position {
+				controller.Lock()
+				crossing.Intersection.LockedBy = -1
+				// notifies everyone an intersection that has become open
+				cond.Broadcast()
+				controller.Unlock()
+			}
+		}
+		time.Sleep(30 * time.Millisecond)
+	}
+}
